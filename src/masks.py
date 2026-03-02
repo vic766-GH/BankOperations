04a54143ec382939f4d7bfe3d6d65c3b607bf1fd
Victor Andreev <766@mail.ru>
2026-03-02 20:58:00 +0500

    настройка .gitignore

---
+++ src/masks.py
@@ -0,0 +1,12 @@
+def get_mask_card_number(card_number: str) -> str:
+    """принимает на вход номер карты и возвращает ее маску
+    :rtype: str
+    """
+
+    return f"{card_number[0:4]} {card_number[4:6]}** **** {card_number[-4:]}"
+
+
+def get_mask_account(card_number: str) -> str:
+    """принимает на вход номер карты и возвращает её краткую маску"""
+
+    return f"**{card_number[-4:]}"
