60369a2b1ea6e2655dbec919ccbfdfb563c16778
Victor Andreev <766@mail.ru>
2026-03-08 19:07:29 +0500

    Удаление остатков лишнего кода из src/masks.py

--- src/masks.py
+++ src/masks.py
@@ -1,7 +1,5 @@
 def get_mask_card_number(card_number: str) -> str:
-    """принимает на вход номер карты и возвращает ее маску
-    :rtype: str
-    """
+    """принимает на вход номер карты и возвращает ее маску"""
 
     return f"{card_number[0:4]} {card_number[4:6]}** **** {card_number[-4:]}"
 
