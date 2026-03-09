7e27ca347ef7cd022c0e8fd2844607985423dd90
Victor Andreev <766@mail.ru>
2026-03-09 22:52:20 +0500

    создание модуля processing.py с новыми функциями

---
+++ src/processing.py
@@ -0,0 +1,18 @@
+def filter_by_state(input_dict_list: list, state: str = 'EXECUTED') -> list:
+    """ Функция принимает на вход список словарей по банковским операциям и возвращает
+        новый список, с выборкой тех, у которых ключ 'state' содержит переданное в функцию значение
+    """
+
+    new_list_with_state = []
+    for operation in input_dict_list:
+        if operation['state'] == state:
+            new_list_with_state.append(operation)
+
+    return new_list_with_state
+
+
+def sort_by_date(dict_list_input: list, descending_order: bool = True) -> list :
+    """ Функция принимает на вход список словарей по банковским операциям и возвращает
+        новый список, отсортированный в заданном вторым параметром порядке
+"""
+    return sorted(dict_list_input, key=lambda x: x['date'], reverse=descending_order)
