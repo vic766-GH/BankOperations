cfeb65eae5710c7e350206881d9d2c7da0b22cc0
Victor Andreev <766@mail.ru>
2026-03-12 19:48:02 +0500

    Создание ветки feature/homework_10_2

--- src/masks.py
+++ src/masks.py
@@ -1,8 +1,9 @@
 def get_mask_card_number(card_number: str) -> str:
     """принимает на вход номер карты и возвращает ее маску"""
-
-    return f"{card_number[0:4]} {card_number[4:6]}** **** {card_number[-4:]}"
-
+    if len(card_number) > 0:
+        return f"{card_number[0:4]} {card_number[4:6]}** **** {card_number[-4:]}"
+    else:
+    return ""
 
 def get_mask_account(card_number: str) -> str:
     """принимает на вход номер карты и возвращает её краткую маску"""
