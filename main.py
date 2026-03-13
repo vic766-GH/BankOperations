ea3042f02c0d909939856f1ecddb8900bf7f7e8f
Victor Andreev <766@mail.ru>
2026-03-13 20:30:57 +0500

    Тест test_masks

--- main.py
+++ main.py
@@ -34,7 +34,6 @@
 # )
 from src.processing import filter_by_state, sort_by_date
 
-
 in_list_dict = [
     {'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
     {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'},
