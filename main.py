a57c4d3ebd6162538def7dacfd59a0baa5616f5b
Victor Andreev <766@mail.ru>
2026-03-14 01:04:20 +0500

    widget.mask_account_card

--- main.py
+++ main.py
@@ -20,32 +20,32 @@
 # print(get_mask_card_number('7000792289606361'))
 # print(get_mask_account('73654108430135874305'))
 
-# from src.widget import mask_account_card, get_date
-#
-# input_data = (
-#     "Maestro 1596837868705199",
-#     "Счет 64686473678894779589",
-#     "MasterCard 7158300734726758",
-#     "Счет 35383033474447895560",
-#     "Visa Classic 6831982476737658",
-#     "Visa Platinum 8990922113665229",
-#     "Visa Gold 5999414228426353",
-#     "Счет 73654108430135874305",
-# )
-from src.processing import filter_by_state, sort_by_date
+from src.widget import mask_account_card, get_date
 
-in_list_dict = [
-    {'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
-    {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'},
-    {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
-    {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}
-]
-state = 'EXECUTED'
-sort_by_descending_order = False
+input_data = (
+    "Maestro 1596837868705199",
+    "Счет 64686473678894779589",
+    "MasterCard 7158300734726758",
+    "Счет 35383033474447895560",
+    "Visa Classic 6831982476737658",
+    "Visa Platinum 8990922113665229",
+    "Visa Gold 5999414228426353",
+    "Счет 73654108430135874305",
+)
+# from src.processing import filter_by_state, sort_by_date
+#
+# in_list_dict = [
+#     {'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
+#     {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'},
+#     {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
+#     {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}
+# ]
+# state = 'EXECUTED'
+# sort_by_descending_order = False
 
 if __name__ == "__main__":
-    # for input_string in input_data:
-    #     print(mask_account_card(input_string))
+    for input_string in input_data:
+        print(mask_account_card(input_string))
     # print(get_date('2024-03-11T02:26:18.671407'))
 
-    print(sort_by_date(filter_by_state(in_list_dict, state), sort_by_descending_order))
+#    print(sort_by_date(filter_by_state(in_list_dict, state), sort_by_descending_order))
--- src/widget.py
+++ src/widget.py
@@ -1,4 +1,4 @@
-from masks import get_mask_account, get_mask_card_number
+from src.masks import get_mask_account, get_mask_card_number
 
 
 def mask_account_card(card_info: str) -> str:
@@ -7,13 +7,32 @@ def mask_account_card(card_info: str) -> str:
     first_space_index = card_info.find(" ")
     last_space_index = card_info.rfind(" ")
 
+    if len(card_info) == 0:
+        return ""
     if card_info[:first_space_index] == "Счет":
-        return f"Счет {get_mask_account(card_info[first_space_index + 1:])}"
+        if len(card_info) ==25:
+            return f"Счет {get_mask_account(card_info[first_space_index + 1:])}"
+        elif len(card_info) > 24: # Некорректные данные - превышено количество цифр в счёте карты
+            return ""
+        elif len(card_info) < 25: # Некорректные данные - недостаточно цифр в счёте карты или отсутствует счёт карты
+            return ""
+        else:
+#            return f"Счет {get_mask_account(card_info[first_space_index + 1:])}"
+            return "OK???"
     else:
-        if first_space_index == last_space_index:
-            return f"{card_info[: first_space_index]} {get_mask_card_number(card_info[first_space_index + 1:])}"
+        if not len(card_info[last_space_index + 1:]) == 16:
+            return ""
+        elif len(card_info) <= 16: # Некорректные данные - отсутствует наименование или номер карты, или ключевое слово
+            return ""
+        elif len(card_info[last_space_index + 1:]) > 16: # Некорректные данные - превышено количество цифр в номере карты
+            return ""
+        elif len(card_info[last_space_index + 1:]) < 16: # Некорректные данные - недостаточно цифр в номере карты
+            return ""
+        elif not card_info[last_space_index + 1:].isdigit():
+            return ""
         else:
             return f"{card_info[: last_space_index]} {get_mask_card_number(card_info[last_space_index + 1:])}"
+
 
 
 def get_date(date_string: str) -> str:
