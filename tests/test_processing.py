b1ff71a6f72b62a25cb89a0f1d62fe483b322872
Victor Andreev <766@mail.ru>
2026-03-13 18:45:27 +0500

    подготовка ко 2-му тесту

---
+++ tests/test_processing.py
@@ -0,0 +1,2 @@
+import pytest
+from src.processing import filter_by_state, sort_by_date
---
+++ tests/test_widget.py
@@ -0,0 +1,6 @@
+import pytest
+
+from src.widget import mask_account_card, get_date
+
+def test_mask_account_card()
+    assert mask_account_card()
