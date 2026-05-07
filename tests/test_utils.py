c00b065f562a2343a435d5b829e656eb05539f7f
Victor Andreev <766@mail.ru>
2026-05-07 23:20:55 +0500

    Домашняя работа 12.1 Работа над ошибками по замечаниям наставника. Включено тестирование get_operations()

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,8 +1,9 @@
+from unittest.mock import Mock
+
 import pytest
 
+from src.utils import get_operations, transaction_to_rub
 
-from unittest.mock import Mock
-from utils import get_operations, transaction_to_rub
 
 @pytest.mark.parametrize(
     "path_to_file, get_dict",
@@ -87,10 +88,13 @@ def test_get_operations_mock() -> None:
 def test_transaction_to_rub(transaction: dict, amount_rub: float) -> None:
     assert transaction_to_rub(transaction) == amount_rub
 
+
 def test_transaction_tu_rub_mock() -> None:
     mock_transaction_tu_rub = Mock(return_value=121.15)
     get_transaction_tu_rub = mock_transaction_tu_rub
-    assert get_transaction_tu_rub({
+    assert (
+        get_transaction_tu_rub(
+            {
                 "id": 41428829,
                 "state": "EXECUTED",
                 "date": "2019-07-03T18:35:29.512364",
@@ -98,14 +102,19 @@ def test_transaction_tu_rub_mock() -> None:
                 "description": "Перевод организации",
                 "from": "MasterCard 7158300734726758",
                 "to": "Счет 35383033474447895560",
-            }) == 121.15
+            }
+        )
+        == 121.15
+    )
     mock_transaction_tu_rub.assert_called()
-    mock_transaction_tu_rub.assert_called_with({
-                "id": 41428829,
-                "state": "EXECUTED",
-                "date": "2019-07-03T18:35:29.512364",
-                "operationAmount": {"amount": "8221.37", "currency": {"name": "USD", "code": "USD"}},
-                "description": "Перевод организации",
-                "from": "MasterCard 7158300734726758",
-                "to": "Счет 35383033474447895560",
-            })
+    mock_transaction_tu_rub.assert_called_with(
+        {
+            "id": 41428829,
+            "state": "EXECUTED",
+            "date": "2019-07-03T18:35:29.512364",
+            "operationAmount": {"amount": "8221.37", "currency": {"name": "USD", "code": "USD"}},
+            "description": "Перевод организации",
+            "from": "MasterCard 7158300734726758",
+            "to": "Счет 35383033474447895560",
+        }
+    )
