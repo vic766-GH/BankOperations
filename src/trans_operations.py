d6dd8725f268196cdaf309e075b87c2c1d6d8e39
Victor Andreev <766@mail.ru>
2026-06-15 02:05:27 +0500

    Домашняя работа 13.2 удалены лишние комментарии из новых модулей

--- src/trans_operations.py
+++ src/trans_operations.py
@@ -1,7 +1,6 @@
 import re
 import json
 import pandas as pd
-import requests
 
 from collections import Counter
 from operator import itemgetter
@@ -37,91 +36,6 @@ def source_select(file_name: str) -> tuple[list,list[dict]]:
 
     answers = []
     answers = get_variant(file_name)
-    # selection = {'1': 'JSON-файла','2': 'CSV-файла','3': 'XLSX-файла'}
-    # if file_name == '':
-    #     file_name = 'data/test_transactions'
-    # print("Привет! Добро пожаловать в программу работы с банковскими транзакциями.")
-    # while True:
-    #     answer_1 = input("""\nВыберите необходимый пункт меню:\n
-    #     1. Получить информацию о транзакциях из JSON-файла
-    #     2. Получить информацию о транзакциях из CSV-файла
-    #     3. Получить информацию о транзакциях из XLSX-файла\n>>>""")
-    #     if answer_1 not in ('1','2','3'):
-    #         if answer_1 in ('q','quit'):
-    #             break
-    #         else:
-    #             print('Вы ошиблись при выборе источника. Повторите ещё раз или нажмите "q" для выхода')
-    #             continue
-    #     else:
-    #         print(f'Вы выбрали получение информации из {selection[answer_1]}\n')
-    #         # if answer_1 == '1':
-    #         #     operations_data = json.load(open(f'{file_name}.json', "r", encoding="UTF-8"))
-    #         #     break
-    #         # elif answer_1 == '2':
-    #         #     operations_data = read_csv(f'{file_name}.csv')
-    #         #     break
-    #         # elif answer_1 == '3':
-    #         #     operations_data = read_xls(f'{file_name}.xlsx')
-    #         #     break
-    #
-    # while True:
-    #     result_list = []
-    #     all_available = True
-    #     print('\nВведите статус, по которому необходимо выполнить фильтрацию.\n')
-    #     answer_2 = input('Доступные для фильтровки статусы: EXECUTED, CANCELED, PENDING\n>>>')
-    #     answer_2_list = re.split(r'[,\s]+', answer_2)
-    #     for answer_2_element in answer_2_list:
-    #         if answer_2_element.upper() not in ('EXECUTED', 'CANCELED', 'PENDING'):
-    #             all_available = False
-    #             print(f'Статус операции "{answer_2_element}" недоступен.')
-    #         # else:
-    #         #      result_list.extend(processing.filter_by_state(operations_data, state=answer_2_element.upper()))
-    #     if all_available:
-    #         break
-    # print(f'Операции отфильтрованы по статусу "{answer_2.upper()}"')
-    #
-    # answer_3 = input('Отсортировать операции по дате? Да/\033[1;4mНет\033[0m\n>>>')
-    # if answer_3.lower() == 'да':
-    #     answer_4 = input('Отсортировать по возрастанию или по убыванию? \033[1;4mпо возрастанию\033[0m/по '
-    #                      'убыванию\n>>>')
-    #     # if answer_4.lower() == 'по возрастанию':
-    #     #     result_list.sort(key=itemgetter('date'))
-    #     # elif answer_4.lower() == 'по убыванию':
-    #     #     result_list.sort(key=itemgetter('date'), reverse=True)
-    #
-    # answer_5 = input('Выводить только рублевые транзакции? Да/\033[1;4mНет\033[0m\n>>>')
-    # # if answer_5.lower() == 'да':
-    # #     result_list_rub = [trans_dict for trans_dict in result_list if trans_dict['currency_code'] == 'RUB']
-    # #     result_list = result_list_rub
-    #
-    # answer_6 = input('Отфильтровать список транзакций по определенному слову в описании? Да/\033[1;4mНет\033[0m\n>>>')
-    # # if answer_6.lower() == 'да':
-    # #     answer_7 = input('Введите слово для поиска\n>>>')
-    # #     result_list = process_bank_search(result_list, state=answer_7)
-
-# Выбор и обработка транзакций по результатам выбора пользователя
-#
-#     if answer_1 == '1':
-#         operations_data = json.load(open(f'{file_name}.json', "r", encoding="UTF-8"))
-#     elif answer_1 == '2':
-#         operations_data = read_csv(f'{file_name}.csv')
-#     else:
-#         operations_data = read_xls(f'{file_name}.xlsx')
-#
-#     result_list.extend(processing.filter_by_state(operations_data, state=answer_2_element.upper()))
-#
-#     if answer_4.lower() == 'по возрастанию':
-#         result_list.sort(key=itemgetter('date'))
-#     elif answer_4.lower() == 'по убыванию':
-#         result_list.sort(key=itemgetter('date'), reverse=True)
-#
-#     if answer_5.lower() == 'да':
-#         result_list_rub = [trans_dict for trans_dict in result_list if trans_dict['currency_code'] == 'RUB']
-#         result_list = result_list_rub
-#
-#     if answer_6.lower() == 'да':
-#         # answer_7 = input('Введите слово для поиска\n>>>')
-#         result_list = process_bank_search(result_list, state=answer_7)
 
     if answers[0] == '1':
         fn = f'{file_name}.json'
@@ -213,14 +127,6 @@ def get_variant(file_name: str) -> list:
                 continue
         else:
             print(f'Вы выбрали получение информации из {selection[answer_1]}\n')
-            # if answer_1 == '1':
-            #     operations_data = json.load(open(f'{file_name}.json', "r", encoding="UTF-8"))
-            #     break
-            # elif answer_1 == '2':
-            #     operations_data = read_csv(f'{file_name}.csv')
-            #     break
-            # elif answer_1 == '3':
-            #     operations_data = read_xls(f'{file_name}.xlsx')
             break
 
     while True:
@@ -233,8 +139,6 @@ def get_variant(file_name: str) -> list:
             if answer_2_element.upper() not in ('EXECUTED', 'CANCELED', 'PENDING'):
                 all_available = False
                 print(f'Статус операции "{answer_2_element}" недоступен.')
-            # else:
-            #      result_list.extend(processing.filter_by_state(operations_data, state=answer_2_element.upper()))
         if all_available:
             break
     print(f'Операции отфильтрованы по статусу "{answer_2.upper()}"')
@@ -244,35 +148,14 @@ def get_variant(file_name: str) -> list:
     if answer_3.lower() == 'да':
                answer_4 = input('Отсортировать по возрастанию или по убыванию? \033[1;4mпо возрастанию\033[0m/по '
                          'убыванию\n>>>')
-        # if answer_4.lower() == 'по возрастанию':
-        #     result_list.sort(key=itemgetter('date'))
-        # elif answer_4.lower() == 'по убыванию':
-        #     result_list.sort(key=itemgetter('date'), reverse=True)
 
     answer_5 = input('Выводить только рублевые транзакции? Да/\033[1;4mНет\033[0m\n>>>')
-    # if answer_5.lower() == 'да':
-    #     result_list_rub = [trans_dict for trans_dict in result_list if trans_dict['currency_code'] == 'RUB']
-    #     result_list = result_list_rub
 
     answer_6 = input('Отфильтровать список транзакций по определенному слову в описании? Да/\033[1;4mНет\033[0m\n>>>')
+
     answer_7 = ''
     if answer_6.lower() == 'да':
        answer_7 = input('Введите слово для поиска\n>>>')
-    #     result_list = process_bank_search(result_list, state=answer_7)
-    #print([answer_1, answer_2_list, answer_3, answer_4, answer_5, answer_6, answer_7])
+
     return [answer_1, answer_2_list, answer_3, answer_4, answer_5, answer_6, answer_7]
 
-
-# тестируемый код
-
-
-def get_user(user_id):
-    response = requests.get(f"https://api.example.com/users/{user_id}")
-    if response.status_code == 200:
-        return response.json()
-    return None
-
-def normalize_dict(in_dict: dict) -> dict:
-    new_list = [{k: '' if v == nan or v == Null else v for k, v in d.items()} for d in in_dict]
-    #print(new_list)
-    return new_list
